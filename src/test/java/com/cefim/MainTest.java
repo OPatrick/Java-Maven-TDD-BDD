3aeba8bbfdf24269551c9634bc2bc3df0d44db5c
patrick <patrick@brouille.net>
2022-02-22 18:14:41 +0100

    Première classe vide et classe de test vide pour vérifier que la mécanique Maven/Junit fonctionne

---
+++ src/test/java/com/cefim/MainTest.java
@@ -0,0 +1,33 @@
+package com.cefim;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/** 
+* Main Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>févr. 21, 2022</pre> 
+* @version 1.0 
+*/ 
+public class MainTest { 
+
+    @BeforeAll
+    public static void before() {
+    }
+
+    @AfterAll
+    public static void after() {
+    }
+
+    /**
+    *
+    * Method: main(String[] args)
+    *
+    */
+    @Test
+    public void testMain() {
+        //TODO: Test goes here...
+    }
+}
